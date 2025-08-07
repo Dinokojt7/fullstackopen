b448d4b97f27f0ef579afe37be8639a082a98433
Tiisetso Dinoko <dinokovonteese@gmail.com>
2025-08-07 12:33:40 +0200

    Make prettier manually

--- part1/causeinfo/src/App.jsx
+++ part1/causeinfo/src/App.jsx
@@ -1,12 +1,10 @@
 const Header = (props) => {
   return (
     <>
-      <h1>
-      {props.course}
-    </h1>
+      <h1>{props.course}</h1>
     </>
-  )
-}
+  );
+};
 
 const Content = (props) => {
   return (
@@ -21,27 +19,28 @@ const Content = (props) => {
         {props.part3} {props.exercises3}
       </p>
     </>
-  )
-}
+  );
+};
 
 const Total = (props) => {
   return (
     <>
       <p>
-        Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+        Number of exercises{" "}
+        {props.exercises1 + props.exercises2 + props.exercises3}
       </p>
     </>
-  )
-}
+  );
+};
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
 
   return (
     <div>
@@ -60,7 +59,7 @@ const App = () => {
         exercises3={exercises3}
       />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
