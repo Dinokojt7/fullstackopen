0d8e331b16f49b2e69ef008f62f34f848ae19984
Tiisetso Dinoko <dinokovonteese@gmail.com>
2025-08-11 15:14:30 +0200

    unicafe step 3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,18 +8,39 @@ const StatsDisplay = (props) => (
   </p>
 );
 
+const Statistics = ({ stats }) => (
+  <div>
+    {stats.map((item, index) => (
+      <StatsDisplay key={index} text={item.text} value={item.value} />
+    ))}
+  </div>
+);
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
   const addGood = () => setGood(good + 1);
-
   const addNeutral = () => setNeutral(neutral + 1);
-
   const addBad = () => setBad(bad + 1);
 
   const total = good + neutral + bad;
+
+  const stats = [
+    { text: "good", value: good },
+    { text: "neutral", value: neutral },
+    { text: "bad", value: bad },
+    { text: "total", value: total },
+    {
+      text: "average",
+      value: total > 0 ? (good * 1 + bad * -1) / total : 0,
+    },
+    {
+      text: "good %",
+      value: total > 0 ? (good / total) * 100 + "%" : "0%",
+    },
+  ];
 
   return (
     <div>
@@ -30,18 +51,7 @@ const App = () => {
       <Button onClick={addBad} text="bad" />
 
       <h2>statistics</h2>
-      <StatsDisplay text="good" value={good} />
-      <StatsDisplay text="neutral" value={neutral} />
-      <StatsDisplay text="bad" value={bad} />
-      <StatsDisplay text="all" value={total} />
-      <StatsDisplay
-        text="average"
-        value={total > 0 ? (good * 1 + neutral * 0 + bad * -1) / total : 0}
-      />
-      <StatsDisplay
-        text="percentage %"
-        value={`${total > 0 ? (good / total) * 100 : 0}%`}
-      />
+      <Statistics stats={stats} />
     </div>
   );
 };
