a9d6a3bb8aaa90086236146ac9f3b0ded17f36f7
Tiisetso Dinoko <dinokovonteese@gmail.com>
2026-05-21 18:45:58 +0200

    Phonebook Backend Step 3

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,42 +1,53 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
-app.get('/api/persons', (request, response) => {
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
 
-    response.json(persons)
-})
-
-app.get('/info', (_request, response) => {
-  const date = new Date()
+app.get("/info", (request, response) => {
+  const date = new Date();
   response.send(
-    `<p>Phonebook has info for ${persons.length} people</p>\n<p>${date}</p>`
-  )
-})
+    `<p>Phonebook has info for ${persons.length} people</p>\n<p>${date}</p>`,
+  );
+});
 
-const PORT = 3001
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+const PORT = 3001;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
