5c44dae4b96a5692f1a8f89e6a29483191e810b6
Tiisetso Dinoko <dinokovonteese@gmail.com>
2026-05-22 20:07:14 +0200

    Phonebook Backend Step 8

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+
+morgan.token("body", (request) => JSON.stringify(request.body));
 
 let persons = [
   {
@@ -25,6 +28,10 @@ let persons = [
 ];
 
 app.use(express.json());
+app.use(morgan("tiny"));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body"),
+);
 
 app.get("/api/persons", (request, response) => {
   response.json(persons);
